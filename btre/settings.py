46c5fdfd49593cc38de027d601c3137ec04d77c5
Corey Bale <33023944+coreybale@users.noreply.github.com>
2019-09-16 14:32:04 +1000

    local setting import

--- btre/settings.py
+++ btre/settings.py
@@ -142,13 +142,8 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-# Email config
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_PORT = 465
-#EMAIL_PORT = 587
-#EMAIL_HOST_USER = ''
-#EMAIL_HOST_PASSWORD = ''
-#EMAIL_USE_TLS = True
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+try:
+    from .local_settings import *
+except ImportError:
+    pass
