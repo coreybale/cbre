97e98638aae65052d5082888956049e2d7e894d8
Corey Bale <33023944+coreybale@users.noreply.github.com>
2019-09-16 10:41:06 +1000

    minoe changes to admin.py pages

--- pages/admin.py
+++ pages/admin.py
@@ -3,8 +3,8 @@ from .models import About
 # Register your models here.
 
 
-class AboutAdmin(admin.ModelAdmin):
-    list_per_page = 25
+# class AboutAdmin(admin.ModelAdmin):
+##   list_per_page = 25
 
 
-admin.site.register(About, AboutAdmin)
+##admin.site.register(About, AboutAdmin)
