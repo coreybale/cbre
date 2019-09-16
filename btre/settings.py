527c179686a6e9de4445a0ff0c4fd48c727f3819
Corey Bale <33023944+coreybale@users.noreply.github.com>
2019-09-16 12:07:27 +1000

    login db changes

--- btre/settings.py
+++ btre/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': 'Cb133a01',
+        'PASSWORD': 'test',
         'HOST': 'localhost'
     }
 }
