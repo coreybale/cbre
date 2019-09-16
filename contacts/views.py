ded33475dffd5eb656c4b96461811d80ba0e0586
Corey Bale <33023944+coreybale@users.noreply.github.com>
2019-09-16 11:35:08 +1000

    email backen config test and minor changes

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 # Create your views here.
@@ -23,13 +24,24 @@ def contact(request):
                 listing_id=listing_id, user_id=user_id)
             if has_contacted:
                 messages.error(
-                    request, 'You have alreasy made an inquiry for this listing.')
+                    request, 'You have already made an inquiry for this listing.')
                 return redirect('/listings/'+listing_id)
 
         contact = Contact(listing=listing, listing_id=listing_id, name=name,
                           email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
+
+        # Send_mail config
+
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for ' + listing +
+            '. Sign into the admin panel for more info.',
+            'csbale@trg.com',
+            ['realtor_email', 'csbale@bgv.com'],
+            fail_silently=False
+        )
 
         messages.success(
             request, 'Your request has been submitted, a realtor will get back to you soon.')
